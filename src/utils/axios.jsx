3d916f98ac4d52373d52c3306dbe606b5a5ebbc4
Anurag Sarkar <sarkaranurag104@gmail.com>
2024-08-21 09:24:46 +0530

    changed baseurl

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance of axios with custom config
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000', // Replace with your base URL
+  baseURL: 'https://room-automation-backend.onrender.com', // Replace with your base URL
   timeout: 10000, // Timeout for requests in milliseconds
 });
 
